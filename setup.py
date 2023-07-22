9b70f7e506ad01114a9be222eb3d777f50910d40
Chunxiao Li <lcx366@gmail.com>
2023-07-22 22:24:26 +0800

    version update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup,find_packages
 
 setup(
     name='starcatalogquery',
-    version='0.1.8',
+    version='0.1.9',
     description='A package to establish an offline star catalog query database',
     author='Chunxiao Li',
     author_email='lcx366@126.com',
